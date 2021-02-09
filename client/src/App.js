c08b0eebb25c3f274e907a13d6b923a15d256ed1
Pedro Barão <phbarao@gmail.com>
2021-02-09 14:49:14 -0300

    implementing template engine ejs

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,20 @@ import api from "./services/api.js";
 export default function App() {
   const [clinic, setClinic] = useState("");
   const [dentist, setDentist] = useState("");
-  const [price1, setPrice1] = useState(0);
-  const [price2, setPrice2] = useState(0);
+  const [price, setPrice] = useState(0);
 
   function createAndDownloadPdf() {
     const today = new Date();
 
     api
-      .post("/create-pdf", { clinic, dentist, price1, price2 })
+      .post("/create-pdf", { clinic, dentist, price })
       .then(() => api.get("/fetch-pdf", { responseType: "blob" }))
       .then((res) => {
         const pdfBlob = new Blob([res.data], { type: "application/pdf" });
 
         saveAs(
           pdfBlob,
-          `${dentist} - ${clinic} (${today.getDate()}_${today.getMonth() +
+          `Recibo ${dentist} - ${clinic} (${today.getDate()}_${today.getMonth() +
             1}_${today.getFullYear()}).pdf`
         );
       });
@@ -46,14 +45,7 @@ export default function App() {
         type="number"
         placeholder="Price 1"
         name="price1"
-        onChange={(e) => setPrice1(e.target.value)}
-      />
-
-      <input
-        type="number"
-        placeholder="Price 2"
-        name="price2"
-        onChange={(e) => setPrice2(e.target.value)}
+        onChange={(e) => setPrice(e.target.value)}
       />
 
       <button onClick={createAndDownloadPdf}>Download PDF</button>
